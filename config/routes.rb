2b12bf147c581692b8eb6c9ed7a2ebd43bab1937
T-oka-4835 <okalovereal35@yahoo.co.jp>
2020-12-14 03:28:43 +0000

    [Add]AdminとCustomerの登録ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :customers, controllers: {
+    sessions: "customers/sessions",
+    passwords: "customers/passwords",
+    registrations: "customers/registrations"
+  }
+
+  devise_for :admins, controllers: {
+    sessions: "admins/sessions",
+    passwords: "admins/passwords",
+    registrations: "admins/registrations"
+  }
+
   root "homes#top"
   get "homes/about" => "homes#about"
 end
