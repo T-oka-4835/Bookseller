71f056e7fd9ea7670d68c7917acc24b0840c7662
T-oka-4835 <okalovereal35@yahoo.co.jp>
2020-12-21 00:48:49 +0000

    [Add]会員ページの作成

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -3,9 +3,9 @@ class Public::CartsController < ApplicationController
 
   def index
     @customer = current_customer
-    @carts = @customer.carts.all
-    @cart = cerrent_customer.carts.find_by(item_id: params[:item_id])
-    @total = 0
+    # @carts = @customer.carts.all
+    # @cart = cerrent_customer.carts.find_by(item_id: params[:item_id])
+    # @total = 0
   end
 
   def create
