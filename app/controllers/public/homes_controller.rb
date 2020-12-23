80c41c8d7caab834a1eaa005defe9592bfac22f8
T-oka-4835 <okalovereal35@yahoo.co.jp>
2020-12-23 13:39:35 +0000

    [Add]homesコントローラーに、current_customerを追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,9 +2,11 @@ class Public::HomesController < ApplicationController
   layout "public-header"
 
   def top
+    @customer = current_customer
   end
 
   def about
+    @customer = current_customer
   end
 
 end
